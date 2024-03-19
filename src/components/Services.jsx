f9c7b3c3c508783d1a80f6d1f713ef4423d2adb5
Gaurav Sharma <gauravsharmatheofficial@gmail.com>
2024-03-19 23:37:44 +0530

    New section added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -17,8 +17,8 @@ const Services = () => {
     <Section id="how-to-use">
       <div className="container">
         <Heading
-          title="Generative AI made for creators."
-          text="Brainwave unlocks the potential of AI-powered applications"
+          title="In Crypto We Trust, But In Uranus, We Believe!"
+          // text="Brainwave unlocks the potential of AI-powered applications"
         />
 
         <div className="relative">
@@ -55,8 +55,8 @@ const Services = () => {
             <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
           </div>
 
-          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
-            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
+          <div className="relative z-1 grid gap-5 lg:grid-cols-1">
+            {/* <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
               <div className="absolute inset-0">
                 <img
                   src={service2}
@@ -76,17 +76,17 @@ const Services = () => {
               </div>
 
               <PhotoChatMessage />
-            </div>
+            </div> */}
 
             <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
               <div className="py-12 px-4 xl:px-8">
-                <h4 className="h4 mb-4">Video generation</h4>
+                <h4 className="h4 mb-4">Video This To know More</h4>
                 <p className="body-2 mb-[2rem] text-n-3">
-                  The world’s most powerful AI photo and video art generation
-                  engine. What will you create?
+                  Elon sues OpenAI, Apple's decline, TikTok ban, Bitcoin $100K?,
+                  Science corner: Microplastics
                 </p>
 
-                <ul className="flex items-center justify-between">
+                {/* <ul className="flex items-center justify-between">
                   {brainwaveServicesIcons.map((item, index) => (
                     <li
                       key={index}
@@ -107,20 +107,28 @@ const Services = () => {
                       </div>
                     </li>
                   ))}
-                </ul>
+                </ul> */}
               </div>
 
-              <div className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]">
-                <img
+              <div className="relative h-[40rem] bg-n-8 rounded-xl overflow-hidden ">
+                {/* <img
                   src={service3}
                   className="w-full h-full object-cover"
-                  width={520}
-                  height={400}
+                  width={3840}
+                  height={2160}
                   alt="Scary robot"
-                />
-
-                <VideoChatMessage />
-                <VideoBar />
+                /> */}
+                <iframe
+                  width="1236"
+                  height="695"
+                  src="https://www.youtube.com/embed/snbTCWL6rxo"
+                  title="E169: Elon sues OpenAI, Apple&#39;s decline, TikTok ban, Bitcoin $100K?, Science corner: Microplastics"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowfullscreen
+                ></iframe>
+                {/* <VideoChatMessage /> */}
+                {/* <VideoBar /> */}
               </div>
             </div>
           </div>
